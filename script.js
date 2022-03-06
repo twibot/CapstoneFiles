dd2684c5986d8b93c0519a517532c131f81dc347
James Li <43506206+twibot@users.noreply.github.com>
2022-03-06 13:30:48 -0600

    Add files via upload
    New Version of JS with working reservation button.

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const container = document.querySelector('.container');
-const parkingSpots = document.querySelectorAll('.row .parking_spots:not(.occupied)')
+const parkingSpots = document.querySelector('.parking_spots');
+const button = document.querySelector('#pushbutton');
+var tmp;
 
-
+//Function takes the container class and add an "click" event. Upon clicking a parking spot, it will be highlighted blue due to the
+//due to the naming of it changing it to parking_spots Selected
 container.addEventListener('click', e => {
   if( e.target.classList.contains('parking_spots') && !e.target.classList.contains('occupied') )
   {
-    e.target.classList.toggle('selected')
+    e.target.classList.toggle('selected');
+    tmp = e.target;
   }
-  
-})
+});
+
+button.addEventListener('click', e => {
+  if(tmp.classList.contains('parking_spots') && tmp.classList.contains('selected'))
+  {
+    tmp.classList.remove('selected');
+    tmp.classList.add('reserved');
+  }
+});
+
